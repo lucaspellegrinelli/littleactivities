36778192f765b5d39cd8c54f206bae6af3558678
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2018-09-19 16:33:47 -0300

    Added code to generate the De Bruijn sequence

---
+++ debruijn.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> de_bruijin(int k, int n){
+  int a[k * n], l = 1;
+  for(int i = 0; i < k * n; i++) a[i] = 0;
+  std::vector<int> sequence;
+
+  while(true){
+    for(int i = 0; n % l == 0 && i < l; i++)
+      sequence.push_back(a[i]);
+    
+    for(int i = l; i < n; i++)
+      a[i] = a[i - l];
+    
+    for(l = n; l > 0 && a[l - 1] >= k - 1; l--);
+
+    if(l == 0) break;
+    a[l - 1]++;
+  }
+
+  for(int i = 0; i < n - 1; i++) sequence.push_back(0);
+  
+  return sequence;
+}
+
+int main() {
+  for(int i : de_bruijin(2, 3)){
+    std::cout << i << " ";
+  }
+}
